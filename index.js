83fd091bce9f1b33dc26d384d371cb448f0f0bf1
Mikhail Samsonov <mikhailsamsonov@Mikhails-MacBook-Air.local>
2023-03-16 16:18:58 +0100

    refactoring

--- index.js
+++ index.js
@@ -1,47 +1,41 @@
 const { Telegraf } = require('telegraf');
-const axios = require('axios');
 const { getInfo } = require('./query');
 
-const bot = new Telegraf('6293036775:AAGaEVUXINoQLDaU13Ge9XuP5GQfKyjku1c');
+const bot = new Telegraf(process.env.TELEGRAM_BOT_KEY);
 
-const kreonId = "5909990042579";
+const kreonId = '5909990042579';
 
 const prepareResult = async () => {
-    const allData = await Promise.all(Array.from(new Array(9), (x, i) => getInfo(kreonId, i + 1)));
-    return allData.reduceRight((acc, data, i) => {
-        return data.reduce((dataAcc, { stock: [{ itemStatus }], pharmacy: { street, buildingNumber, geoLocation } }) => {
-            const key = `${street} ${buildingNumber}`;
-            if (dataAcc[key]?.amount) { return dataAcc };
-            return {
-                ...dataAcc, 
-                [key]: {
-                    geo: geoLocation,
-                    amount: itemStatus === 'AVAILABLE' ? i + 1 : null,
-                }
-            };
-        }, acc)
-    }, {})
-}
+  const allData = await Promise.all(Array.from({ length: 9 }, (_, i) => getInfo(kreonId, i + 1)));
+  return allData.reduceRight((acc, data, i) =>
+    data.reduce((dataAcc, { stock: [{ itemStatus }], pharmacy: { street, buildingNumber, geoLocation } }) => {
+      const key = `${street} ${buildingNumber}`;
+      if (dataAcc[key]?.amount) return dataAcc;
+      return { ...dataAcc, [key]: { geo: geoLocation, amount: itemStatus === 'AVAILABLE' ? i + 1 : null } };
+    }, acc),
+    {}
+  );
+};
 
 const buildGoogleMapsURL = async () => {
-    const result = await prepareResult();
-    const baseUrl = "http://localhost:8080?coords=";
-    const markers = Object.values(result).map(({geo: {latitude: lat, longitude: lng}, amount: tooltip}) => ({ lat, lng, tooltip }))
-    const markerStrings = markers.map(marker => `${marker.lat},${marker.lng},${encodeURIComponent(marker.tooltip)}`);
-    return baseUrl + markerStrings.join(";");
-}
+  const result = await prepareResult();
+  const baseUrl = process.env.MAP_SITE_LINK;
+  const markers = Object.values(result).map(({ geo: { latitude: lat, longitude: lng }, amount: tooltip }) => ({ lat, lng, tooltip }));
+  const markerStrings = markers.map(marker => `${marker.lat},${marker.lng},${encodeURIComponent(marker.tooltip)}`);
+  return baseUrl + markerStrings.join(';');
+};
 
-bot.start(async (ctx) => {
-    const result = await prepareResult();
-    ctx.reply(Object.entries(result).map(([key, value]) => `${key}: ${value.amount}`).join("\n"));
+bot.start(async ctx => {
+  const result = await prepareResult();
+  ctx.reply(Object.entries(result).map(([key, value]) => `${key}: ${value.amount}`).join('\n'));
 });
 
-bot.command('map', async (ctx) => {
-    ctx.reply(await buildGoogleMapsURL());
+bot.command('map', async ctx => {
+  ctx.reply(await buildGoogleMapsURL());
 });
 
 bot.launch();
 
-process.on('uncaughtException', (err) => {
-    console.error('Uncaught exception:', err);
-  });
+process.on('uncaughtException', err => {
+  console.error('Uncaught exception:', err);
+});
--- query.js
+++ query.js
@@ -3,40 +3,45 @@ const { request, gql } = require('graphql-request');
 const endpoint = 'https://api.receptagemini.pl/graphql/';
 
 const query = gql`
-    query getMedicineStockQuery($items: [ItemInput]) {
-        searchInPharmacy(items: $items) {
-            stockByPharmacy: StockByPharmacy {
-                stock {
-                    itemStatus
-                }
-                pharmacy {
-                    street
-                    buildingNumber
-                    city
-                    geoLocation {
-                        latitude
-                        longitude
-                    }
-                }
-            }
+  query getMedicineStockQuery($items: [ItemInput]) {
+    searchInPharmacy(items: $items) {
+      stockByPharmacy {
+        stock {
+          itemStatus
         }
+        pharmacy {
+          street
+          buildingNumber
+          city
+          geoLocation {
+            latitude
+            longitude
+          }
+        }
+      }
     }
+  }
 `;
 
-const getInfo = async (pharmId, amount) => {
-    const variables = {
-        items: [
-            {
-                ean: pharmId,
-                qty: amount
-            }
-        ]
-    }
-    const data = await request(endpoint, query, variables)
+const getInfo = async (drugId, amount) => {
+  const variables = {
+    items: [
+      {
+        ean: drugId,
+        qty: amount
+      }
+    ]
+  };
 
-    return data.searchInPharmacy.stockByPharmacy.filter(({ stock: [{ itemStatus }], pharmacy: { city } }) => itemStatus === 'AVAILABLE' && city === 'Kraków')
-}
+  try {
+    const data = await request(endpoint, query, variables);
+    return data.searchInPharmacy.stockByPharmacy.filter(({ stock: [{ itemStatus }], pharmacy: { city } }) => itemStatus === 'AVAILABLE' && city === 'Kraków');
+  } catch (error) {
+    console.error(`Failed to fetch data for drug ${drugId} with error: ${error.message}`);
+    return [];
+  }
+};
 
 module.exports = {
-    getInfo
+  getInfo
 };
